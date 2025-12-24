0468586e24958900c0074cf5ef877d15d6d6c651
junios-study <junios@naver.com>
2025-12-24 11:30:52 +0900

    스레드 클라이언트 작업

---
+++ client.cpp
@@ -0,0 +1,75 @@
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
+#define _CRT_SECURE_NO_WARNINGS
+
+
+#include <winsock2.h>
+#include <Windows.h>
+#include <process.h>
+#include <iostream>
+
+#pragma comment(lib, "ws2_32")
+
+using namespace std;
+
+
+unsigned RecvThread(void* Arg)
+{
+	SOCKET ServerSocket = *(SOCKET*)Arg;
+
+	char Buffer[1024] = { 0, };
+
+	while (true)
+	{
+		int RecvBytes = recv(ServerSocket, Buffer, sizeof(Buffer), 0);
+
+		cout << Buffer << endl;
+	}
+	return 0;
+}
+
+unsigned SendThread(void* Arg)
+{
+	SOCKET ServerSocket = *(SOCKET*)Arg;
+
+	char Buffer[1024] = { 0, };
+
+	while (true)
+	{
+		cout << "Chat : ";
+		cin.getline(Buffer, sizeof(Buffer));
+		int SendBytes = send(ServerSocket, Buffer, (int)strlen(Buffer) + 1, 0);
+	}
+	return 0;
+}
+
+
+int main()
+{
+	WSAData wsaData;
+
+	WSAStartup(MAKEWORD(2, 2), &wsaData);
+
+	SOCKET ServerSocket = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+	SOCKADDR_IN ServerSockAddr;
+	memset(&ServerSockAddr, 0, sizeof(ServerSockAddr));
+	ServerSockAddr.sin_family = AF_INET;
+	ServerSockAddr.sin_addr.s_addr = inet_addr("112.171.99.20");
+	ServerSockAddr.sin_port = htons(33333);
+
+	int Result = connect(ServerSocket, (SOCKADDR*)&ServerSockAddr, sizeof(ServerSockAddr));
+
+	HANDLE SocketThread[2];
+	SocketThread[0] = (HANDLE)_beginthreadex(0, 0, RecvThread, &ServerSocket, 0, 0);
+	SocketThread[1] = (HANDLE)_beginthreadex(0, 0, SendThread, &ServerSocket, 0, 0);
+
+	DWORD HResult = WaitForMultipleObjects(2, SocketThread, true, INFINITE);
+
+	CloseHandle(SocketThread[0]);
+	CloseHandle(SocketThread[1]);
+
+	closesocket(ServerSocket);
+
+	WSACleanup();
+
+	return 0;
+}
